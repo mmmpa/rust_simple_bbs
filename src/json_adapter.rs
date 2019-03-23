dc4abcffc9f34a49e8c87e79f980cb55bdaaec16
mmmpa <mmmpa.mmmpa@gmail.com>
2019-03-23 11:32:34 +0900

    updated

--- src/json_adapter.rs
+++ src/json_adapter.rs
@@ -1,22 +1,17 @@
 use crate::data_gateway_adapter::{RawThread, DataGatewayAdapter, MessageCreationParams, ThreadCreationParams, RawBoard, BoardCreationParams};
 use std::ops::Range;
 use crate::test_adapter::TestAdapter;
-use std::sync::{Arc, RwLock};
 
 #[derive(Debug)]
 pub struct JsonAdapter {
     auto_sweeping: bool,
-    adapter: Arc<RwLock<TestAdapter>>,
+    adapter: TestAdapter,
 }
 
 impl JsonAdapter {
     pub fn new(path: &str, auto_sweeping: bool) -> JsonAdapter {
         JsonAdapter {
-            adapter: Arc::new(
-                RwLock::new(
-                    TestAdapter::new(path, auto_sweeping)
-                )
-            ),
+            adapter: TestAdapter::new(path, auto_sweeping),
             auto_sweeping,
         }
     }
@@ -24,26 +19,26 @@ impl JsonAdapter {
 
 impl DataGatewayAdapter for JsonAdapter {
     fn show_board(&self, board_id: &str) -> Result<RawBoard, String> {
-        Ok(self.adapter.read().unwrap().show_board(board_id)?)
+        Ok(self.adapter.show_board(board_id)?)
     }
 
     fn show_thread(&self, board_id: &str, thread_id: &str, range: Range<usize>) -> Result<RawThread, String> {
-        Ok(self.adapter.read().unwrap().show_thread(board_id, thread_id, range)?)
+        Ok(self.adapter.show_thread(board_id, thread_id, range)?)
     }
 
     fn create_board(&mut self, params: BoardCreationParams<'_>) -> Result<String, String> {
-        Ok(self.adapter.write().unwrap().create_board(params)?)
+        Ok(self.adapter.create_board(params)?)
     }
 
     fn create_thread(&mut self, params: ThreadCreationParams<'_>) -> Result<String, String> {
-        Ok(self.adapter.write().unwrap().create_thread(params)?)
+        Ok(self.adapter.create_thread(params)?)
     }
 
     fn create_message(&mut self, params: MessageCreationParams<'_>) -> Result<String, String> {
-        Ok(self.adapter.write().unwrap().create_message(params)?)
+        Ok(self.adapter.create_message(params)?)
     }
 
     fn lock_thread(&mut self, board_id: &str, thread_id: &str) -> Result<(), String> {
-        Ok(self.adapter.write().unwrap().lock_thread(board_id, thread_id)?)
+        Ok(self.adapter.lock_thread(board_id, thread_id)?)
     }
 }
